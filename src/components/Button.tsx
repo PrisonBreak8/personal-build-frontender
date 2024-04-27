336bf83697bc8e05dda7741166343b858a0583b2
PrisonBreak8 <golubev.artur.1986@gmail.com>
2024-04-27 16:07:58 +0300

    new file:   rollup.config.js

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { ReactNode } from "react";
+
+type PropsType = {
+	children: ReactNode;
+	onClick?: () => void;
+};
+const Button = ({ children, onClick }: PropsType) => {
+	return (
+		<button
+			onClick={onClick}
+			style={{
+				padding: "10px 20px",
+				fontSize: "1.2em",
+				borderRadius: "5px",
+				cursor: "pointer",
+				backgroundColor: "blue",
+				color: "white",
+				border: "none",
+			}}
+		>
+			{children}
+		</button>
+	);
+};
+
+export { Button };
